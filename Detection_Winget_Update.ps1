5248452e09902ab439919cc6af4d52e236a36f03
Pavel Mirochnitchenko <43411053+yannara@users.noreply.github.com>
2026-02-20 12:39:07 +0200

    Delete Detection_Winget_Update.ps1

--- Detection_Winget_Update.ps1
+++
@@ -1,9 +0,0 @@
-﻿if (Test-Path -Path "C:\ProgramData\Microsoft\IntuneManagementExtension\Logs\winget_update_any_apps_v2.6.log") {
-    # File exists – considered compliant, do nothing
-    Write-Output "File exists. No remediation needed."
-    exit 0
-} else {
-    # File does not exist – considered non-compliant
-    Write-Output "File does not exist. Remediation required."
-    exit 1
-}
